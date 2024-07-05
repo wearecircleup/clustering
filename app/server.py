a013759327e6cca4353a15310e84031e2684f553
daniel.munevar <daniel.munevar@rappi.com>
2024-07-05 11:03:32 -0500

    PORT Remove as EnvVariable

--- app/server.py
+++ app/server.py
@@ -123,5 +123,4 @@ async def predict(user_data: UserData):
 
 if __name__ == "__main__":
     import uvicorn
-    port = int(os.environ.get("PORT", 8000))
-    uvicorn.run("server:app", host="0.0.0.0", port=port)
+    uvicorn.run("app.server:app", host="0.0.0.0", port=8000)
