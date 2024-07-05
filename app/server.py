f3421021e5d63cd5c5543944c226a39684a9fe92
daniel.munevar <daniel.munevar@rappi.com>
2024-07-05 10:57:41 -0500

    add PORT env variable

--- app/server.py
+++ app/server.py
@@ -7,8 +7,6 @@ import numpy as np
 import os
 import uvicorn
 
-port = int(os.getenv("PORT", 8000))
-
 app = FastAPI()
 
 try:
@@ -125,4 +123,5 @@ async def predict(user_data: UserData):
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run("app.server:app", host="0.0.0.0", port=port, reload=True)
+    port = int(os.environ.get("PORT", 8000))
+    uvicorn.run("server:app", host="0.0.0.0", port=port)
